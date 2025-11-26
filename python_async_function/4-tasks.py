53ad4132e7e0711e6c9c958afcd23f8b4fe627cf
Dyasteek <lawarsenik@gmail.com>
2025-11-26 16:35:13 -0300

    [fix]: trying to pass pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -17,7 +17,8 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawn task_wait_random n times 
+    Spawn task_wait_random n times with the specified max_delay
+    Return the list of all delays in ascending order
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
