b91d3cfe0d5b9973e570faf354a9e9a8fb8f1556
Dyasteek <lawarsenik@gmail.com>
2025-11-26 16:33:56 -0300

    [fix]: trying to pass pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,16 +10,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Concurrent coroutines
+    Spawn wait_random n times with the specified max_delay
+    Return the list of all delays in ascending order
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
-    
+
     for coro in asyncio.as_completed(tasks):
         delay = await coro
         i = 0
         while i < len(delays) and delay > delays[i]:
             i += 1
         delays.insert(i, delay)
-    
+
     return delays
