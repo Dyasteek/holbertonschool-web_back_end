560595be40dbafbe19d7a51d982f38b44a480594
Dyasteek <lawarsenik@gmail.com>
2025-11-26 15:27:04 -0300

    [fix]: add asyncio.sleep to wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,6 +2,7 @@
 """
 Basic async syntax
 """
+import asyncio
 import random
 
 
@@ -9,4 +10,6 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     Wait for a random delay
     """
-    return random.uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
