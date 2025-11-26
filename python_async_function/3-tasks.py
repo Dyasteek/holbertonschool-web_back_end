87a5c9496d57ce5fe313521615aff7e6f5231999
Dyasteek <lawarsenik@gmail.com>
2025-11-26 16:16:16 -0300

    [fix]: added a blank line at line 10

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -7,6 +7,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create a task for wait_random
