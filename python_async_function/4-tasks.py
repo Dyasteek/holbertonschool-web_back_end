84ace63e0b58dc8f2860ee9705055b1c4842cf57
Dyasteek <lawarsenik@gmail.com>
2025-11-26 16:27:25 -0300

    [fix]: added fun task_wait_n

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,8 +7,26 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create a task for wait_random
     """
     return asyncio.create_task(wait_random(max_delay))
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn task_wait_random n times 
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        i = 0
+        while i < len(delays) and delay > delays[i]:
+            i += 1
+        delays.insert(i, delay)
+    
+    return delays
