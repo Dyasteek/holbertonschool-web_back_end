8275eab035c4aa53bc77c029e70721a4b73c351e
Dyasteek <lawarsenik@gmail.com>
2025-11-26 15:08:34 -0300

    [add]: function tat takes a float multipilier as argument and returns a function that multiplies a float by multiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Make a multiplier function
+"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Make a multiplier function
+    """
+    return lambda x: x * multiplier
