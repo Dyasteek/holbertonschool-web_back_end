d58b14dcd5258188b346d738e535cfcd4675dbad
Dyasteek <lawarsenik@gmail.com>
2025-11-26 16:30:25 -0300

    [fix]: solved some blank spaces

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -21,12 +21,12 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
-    
+
     for task in asyncio.as_completed(tasks):
         delay = await task
         i = 0
         while i < len(delays) and delay > delays[i]:
             i += 1
         delays.insert(i, delay)
-    
+
     return delays
